b78445d98f1b68804bfde1a24ff518e812f7dafa
Jack Wright <Jack_Wright@brown.edu>
2019-01-26 21:24:40 -0500

    jcw update description

--- R/CLPS0900.R
+++ R/CLPS0900.R
@@ -1,8 +1,12 @@
 #' CLPS0900R: Utilities for Quantitative Methods course.
 #'
 #' The package includes demonstrations in R that are presented in class,
-#' and tools that are used in weekly problem sets.
+#' tools that are used in weekly problem sets, and associated data sets.
 #'
+#' \describe{
+#'   \item{demo_hist()}{A simple histogram}
+#'   \item{demo_nhst()}{A simulation of 2-sample comparisons}
+#' }
 #'
 #' @docType package
 #' @name CLPS0900R

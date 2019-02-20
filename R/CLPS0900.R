4354a54807f82520613cde418a01ec1cea9f5146
Jack Wright <Jack_Wright@brown.edu>
2019-02-20 15:23:32 -0500

    fixing package documentation

--- R/CLPS0900.R
+++ R/CLPS0900.R
@@ -3,16 +3,23 @@
 #' The package includes demonstrations in R that are presented in class,
 #' tools that are used in weekly problem sets, and associated data sets.
 #'
-#' \describe{
-#'   \item{demo_hist()}{A simple histogram}
-#'   \item{demo_nhst()}{A simulation of 2-sample comparisons}
-#'   \item{sdpop(x)}{Returns sigma for an assumed population of values}
-#'   \item{binomials1()}{Shows binomial distribution}
-#'   \item{run.binomials1()}{Run binomial demonstration with manipulate wrapper}
-#'   \item{samp.dist1()}{Show a sampling distribution of the mean}
-#'   \item{run.sampling.dist1()}{Run sampling distribution with manipulate wrapper}
-#'   \item{histnormal()}{Create density histogram with normal superimposed}
-#' }
+#'
+#' @section CLPS0900R functions:
+#' demo_hist()  A simple histogram
+#'
+#' demo_nhst()  A simulation of 2-sample comparisons
+#'
+#' sdpop(x)  Returns sigma for an assumed population of values
+#'
+#' binomials1()  Shows binomial distribution
+#'
+#' run.binomials1()  Run binomial demonstration with manipulate wrapper
+#'
+#' samp.dist1()  Show a sampling distribution of the mean
+#'
+#' run.sampling.dist1()  Run sampling distribution with manipulate wrapper
+#'
+#' histnormal()  Create density histogram with normal superimposed
 #'
 #' @docType package
 #' @name CLPS0900R

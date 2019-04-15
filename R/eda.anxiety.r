69388cbee0ba51303d325bd825558a0657d5527b
Jack Wright <Jack_Wright@brown.edu>
2019-04-15 16:47:58 -0400

    unrequiring packages

--- R/eda.anxiety.r
+++ R/eda.anxiety.r
@@ -22,27 +22,48 @@ eda.anxiety <- function(x=NULL,show.hist=TRUE,show.summary=FALSE,show.anova=FALS
                           plot.main.gender=FALSE,plot.main.condition=FALSE,plot.interaction=FALSE,
                           show.tukey=FALSE){
 
-  #require(foreign)
-  #require(phia)
-  #require(FSA)
-
- if(is.null(x)){
-  MBSR_DATA = read.csv("MBSR.data.csv")
-  x <- MBSR_DATA
- }
+  #require psych
 
   par(mfrow=c(2,2))
   par(mai=c(.7,.8,.5,.2))
   cat("\014")
 
+  if(is.null(x)){
+    if(file.exists("MBSR.data.csv")){
+      MBSR_DATA = read.csv("MBSR.data.csv")
+      x <- MBSR_DATA
+      assign("MBSR_DATA",x,pos=1)
+    }
+    else {
+      stop("\n
+As noted in the instructions for this assignment, you
+must create a data file that contains the data, and it
+must be named MBSR.data.csv.  This is to help you learn
+how data in a 2 x 2 factorial design are typically
+formatted for this type of ANOVA.\n\n",call.=FALSE)
+    }
+  }
+
   if(show.hist==TRUE){
+    cat("\n_______________________________________________________________________\n")
+    cat("\n
+As noted in the instructions for this assignment, our sample size
+in this hypothetical data set is very small.  The histogram and normal
+quantile plot are displayed here only to initialize your plot frame.\n")
+    cat("\n_______________________________________________________________________\n\n")
+
    hist(x$anxiety,col="tan",main="Histogram",xlab="Anxiety")
    qqnorm(x$anxiety,main="Normal Quantile Plot")
   }
 
-  Sum = Summarize(anxiety ~ gender * condition, data=x, digits=2)
-  Sum$se = Sum$sd / sqrt(Sum$n)
-  Sum$se = signif(Sum$se, digits=3)
+  dout <- describeBy(x,list(x$gender,x$condition),mat=TRUE)
+  dout <- dout[-c(1:8),c(2,3,5,6,7)]
+  variance <- round(dout$sd^2,3)
+  se <- dout$sd/sqrt(dout$n)
+  dout$sd <- round(dout$sd,2)
+  dout2 <- data.frame(gender=dout$group1,condition=dout$group2,n=dout$n,
+                      mean=dout$mean,sd=dout$sd,variance=variance,se=se)
+
   anova.result <- aov(anxiety ~ gender * condition,data=x)
 
   if(show.summary==TRUE){
@@ -53,19 +74,25 @@ eda.anxiety <- function(x=NULL,show.hist=TRUE,show.summary=FALSE,show.anova=FALS
    cat("\n")
 
    cat("\nSummary of Data, by Gender and Condition:\n\n")
-   print(Sum,digits=3)
+   print(dout2)
+
    cat("\n__________________________________________________________________\n")
   }
 
   ylimits <- c(10,22)
 
   if(plot.main.gender==TRUE){
-    IM = interactionMeans(anova.result,factors="gender")
-    ym <- IM$adj
-    se <- IM$std
+    ym <- tapply(x$anxiety,INDEX=x$gender,FUN="mean")
+    ysd <- tapply(x$anxiety,INDEX=x$gender,FUN="sd")
+    yn <- tapply(x$anxiety,INDEX=x$gender,FUN="length")
+    var.pool <- dout2$variance
+    sd.pool <- sqrt(mean(var.pool))
+    yse <- rep(sd.pool,2)/sqrt(yn)
+    xt <- cbind(ym,ysd,yn,yse)
     xcor <- c(1,2)
-    ylo <- ym - 2*se
-    yhi <- ym + 2*se
+
+    ylo <- ym - 2*yse
+    yhi <- ym + 2*yse
 
     plot(xcor,ym,type="n",main="Gender Main Effect",xlab="",ylab="Mean Anxiety",xaxt="n",
          ylim=ylimits,xlim=c(.75,2.25))
@@ -73,15 +100,22 @@ eda.anxiety <- function(x=NULL,show.hist=TRUE,show.summary=FALSE,show.anova=FALS
     points(xcor,ym,pch=19,cex=1.5)
     lines(xcor,ym,pch=19,col="black",lty=1,lwd=2)
     segments(xcor,ylo,xcor,yhi,lty=1,lwd=1)
+
   }
 
   if(plot.main.condition==TRUE){
-    IM = interactionMeans(anova.result,factors="condition")
-    ym <- IM$adj
-    se <- IM$std
+
+    ym <- tapply(x$anxiety,INDEX=x$condition,FUN="mean")
+    ysd <- tapply(x$anxiety,INDEX=x$condition,FUN="sd")
+    yn <- tapply(x$anxiety,INDEX=x$condition,FUN="length")
+    var.pool <- dout2$variance
+    sd.pool <- sqrt(mean(var.pool))
+    yse <- rep(sd.pool,2)/sqrt(yn)
+    xt <- cbind(ym,ysd,yn,yse)
+
     xcor <- c(1,2)
-    ylo <- ym - 2*se
-    yhi <- ym + 2*se
+    ylo <- ym - 2*yse
+    yhi <- ym + 2*yse
 
     plot(xcor,ym,type="n",main="Treatment Main Effect",xlab="",ylab="Mean Anxiety",xaxt="n",
          ylim=ylimits,xlim=c(.75,2.25))
@@ -94,30 +128,25 @@ eda.anxiety <- function(x=NULL,show.hist=TRUE,show.summary=FALSE,show.anova=FALS
   if(plot.interaction==TRUE){
 
     ylimits <- c(5,30)
-
-    IM = interactionMeans(anova.result)
     xcor <- c(1,2)
+    IM.f <-dout2[dout2$gender=="F",]
+    ym.f <- IM.f$mean
+    se.f <- IM.f$se
+    ylo.f <- ym.f - 2*se.f
+    yhi.f <- ym.f + 2*se.f
 
-    IM.f <-IM[IM$gender=="F",]
-    ym.f <- IM.f$adj
-    se.f <- IM.f$std
-    ylo.f <- ym.f - 2*se
-    yhi.f <- ym.f + 2*se
-
-    IM.m <-IM[IM$gender=="M",]
-    ym.m <- IM.m$adj
-    se.m <- IM.m$std
-    ylo.m <- ym.m - 2*se
-    yhi.m <- ym.m + 2*se
+    IM.m <-dout2[dout2$gender=="M",]
+    ym.m <- IM.m$mean
+    se.m <- IM.m$se
+    ylo.m <- ym.m - 2*se.m
+    yhi.m <- ym.m + 2*se.m
 
     plot(xcor,c(10,20),type="n",main="Gender x Treatment",xlab="",ylab="Mean Anxiety",xaxt="n",
          ylim=ylimits,xlim=c(.75,2.25))
     axis(at=xcor,labels=c("Control","MBSR"),side=1)
-
     points(xcor,ym.f,pch=19,cex=1.5)
     lines(xcor,ym.f,pch=19,col="black",lty=1,lwd=2)
     segments(xcor,ylo.f,xcor,yhi.f,lty=1,lwd=1)
-
     points(xcor,ym.m,pch=19,cex=1.5)
     lines(xcor,ym.m,pch=19,col="red",lty=3,lwd=2)
     segments(xcor,ylo.m,xcor,yhi.m,lty=1,lwd=1)
@@ -136,7 +165,8 @@ eda.anxiety <- function(x=NULL,show.hist=TRUE,show.summary=FALSE,show.anova=FALS
   if(show.tukey==TRUE){
     cat("\n__________________________________________________________________\n\n")
     print(TukeyHSD(anova.result,which="gender:condition"))
-    cat("Comparisons with /p adj/ < .05 exceed HSD for alpha = .05.\n")
+    cat("Comparisons with /p adj/ < .05 exceed critical HSD for
+experiment-wise alpha = .05.\n\n")
     cat("__________________________________________________________________\n\n")
   }
 

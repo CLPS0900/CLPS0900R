42d63228568fcb8e1d6769d6af1dd064183b84cc
Jack Wright <Jack_Wright@brown.edu>
2020-02-20 10:00:39 -0500

    updating samp.dist

--- R/run.sampling.dist1.r
+++ R/run.sampling.dist1.r
@@ -23,7 +23,8 @@ run.sampling.dist1 <- function() {
              ssize=slider(1,100,step=1,initial=1,label="Sample Size (N)"),
              pop.type=picker("Uniform[1-6]","Uniform[1-10]","Binomial","WAIS IQ",
                              "Sunspot Activity","Old Faithful","World IPP","US Violent Crime",
-                             "Custom",label="Population Type"),
+                             "Neuroticism-All","Neuroticism-Men","Neuroticism-Women",
+                             label="Population Type"),
              add.normal=checkbox(label="Add Normal Distribution")
   )
 }
